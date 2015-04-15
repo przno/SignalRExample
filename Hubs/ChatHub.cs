3c0ed5051a779c46afe56942b28b934bc3b3e17c
Martin Praznovsky <martin.praznovsky@gmail.com>
2015-04-15 21:50:41 +0200

    initial

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRExample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string userName, string message)
+        {
+            string timeNow = DateTime.Now.Hour + ":" + DateTime.Now.Minute.ToString("D2") + ":" + DateTime.Now.Second.ToString("D2");
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(userName, message, timeNow);
+        }
+    }
+}
